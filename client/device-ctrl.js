da28816e5d3e40d07a0b5ab25d848545f630683e
Bjørge Næss <bjoerge@bengler.no>
2014-01-19 23:27:39 +0100

    Finish implementation of gesture based dimming w/hammer

--- client/device-ctrl.js
+++ client/device-ctrl.js
@@ -7,22 +7,51 @@ var api = require("./api")
 var Hammer = require("hammerjs")
 
 var Dimmer = React.createClass({
-  getInitialState: function() {
-    return {adjustingLevel: 0};
+  componentDidMount: function() {
+    this.getDOMNode().style.width="100%";
+    this.availWidth = this.getDOMNode().offsetWidth;
+    this.getDOMNode().style.width = this.toPercentage(this.getLevel())+"%";
+    this.hammer = Hammer(this.getDOMNode());
+    this.hammer.on('dragstart', this.toggleDrag);
+    this.hammer.on('dragend', this.toggleDrag);
+    this.hammer.on('drag', this.drag);
   },
-  dim: function(e) {
-    var level = Number(e.target.value)
-    this.setState({adjustingLevel: level})
-    this.props.onDim(level);
+  componentWillUnmount: function() {
+    this.hammer.off('dragstart', this.toggleDrag);
+    this.hammer.off('dragend', this.toggleDrag);
+    this.hammer.off('drag', this.drag );
   },
   componentWillReceiveProps: function() {
     this.setState({adjustingLevel: 0});
   },
+  toggleDrag: function() {
+    if (!this.dragging) {
+      this.dragStartWidth = this.getDOMNode().offsetWidth;
+      this.dragging = true;
+    }
+    else {
+      this.dragStartWidth = null;
+      this.dragging = false;
+    }
+  },
+  drag: function(event) {
+    var gesture = event.gesture;
+    var normalizedLevel = this.normalizeLevel(this.dragStartWidth+gesture.deltaX); 
+    this.getDOMNode().style.width = this.toPercentage(normalizedLevel)+"%";
+    this.props.onDim(normalizedLevel)
+  },
+  normalizeLevel: function(level) {
+    level = level < 0 ? 0 : level > this.availWidth ? this.availWidth : level;
+    return (255 / this.availWidth) * level
+  },
+  getLevel: function() {
+    return this.props.state === 'DIM' ? this.props.level : this.props.state === 'ON' ? 255 : 0
+  },
+  toPercentage: function(level) {
+    return ((100/255)*level).toFixed()
+  },
   render: function() {
-    var level = this.state.adjustingLevel || (this.props.state === 'DIM' ? this.props.level : this.props.state === 'ON' ? 255 : 0);
-    return <span>
-        <input type="range" min="1" max="255" step="1" value={level} onChange={this.dim}/> {((100/255)*level).toFixed()}%
-        </span>
+    return <span className="divider"></span>
   }
 });
 
@@ -30,10 +59,16 @@ var PowerButton = React.createClass({
   toggle: function(e) {
     this.props.onChange(!this.props.isOn);
   },
+  componentDidMount: function() {
+    this.hammer = Hammer(this.getDOMNode());
+    this.hammer.on('tap', this.toggle );
+  },
+  componentWillUnmount: function() {
+    this.hammer.off('tap', this.toggle );
+  },
   render: function() {
-    var label = this.props.isOn ? "Turn OFF" :  "Turn ON"
     var classes = ['default', this.props.isOn ? 'on' : 'off'];
-    return <input type="checkbox" className={classes.join(" ")} onClick={this.toggle} />
+    return <input type="checkbox" className={classes.join(" ")}/>
   }
 });
 
@@ -52,47 +87,18 @@ module.exports = React.createClass({
     }.bind(this))
   },
 
-  drag: function() {
-    // Dims the device on dragging according to the width of the element
-    console.log( event.gesture );
-    var g = event.gesture
-    var d = g.interimDirection;
-    
-    console.log( g.deltaX, window.outerWidth ); // or the width of the element
-
-    // on start drag (or hold) show the divider (set opacity)
-
-    if ( d == "left") {
-
-      console.log("left");
-
-    } else if( d == "right" ) {
-
-      console.log("right");
-
-    }
+  isOn: function() {
+    return this.state.status.name == 'ON' || this.state.status.name == 'DIM';
   },
-
-  tap: function() {
-    // turn the light on/off
-  },
-
-  componentDidMount: function() {
-    this.hammer = Hammer(this.getDOMNode())
-    this.hammer.on('drag', this.drag);
-    this.hammer.on('tap', this.tap );
-  },
-  componentWillUnmount: function() {
-    this.hammer.off('drag', this.drag);
-    this.hammer.off('tap', this.tap);
+  isDimmable: function() {
+    return this.state.methods.indexOf('DIM') > -1;
   },
   render: function() {
     var controls = [];
-    var isOn = this.state.status.name == 'ON' || this.state.status.name == 'DIM';
 
-    controls.push(<PowerButton isOn={isOn} onChange={this.onPowerToggle.bind(this, this.state.id)}/>);
+    controls.push(<PowerButton isOn={this.isOn()} onChange={this.onPowerToggle.bind(this, this.state.id)}/>);
 
-    if (this.state.methods.indexOf('DIM') > -1) {
+    if (this.isDimmable()) {
       controls.push(<Dimmer level={this.state.status.level} state={this.state.status.name} onDim={this.dim.bind(this, this.state.id)}/>);
     }
     var name = this.state.status.name == "DIM" ? 'ON' : this.state.status.name;
@@ -102,8 +108,6 @@ module.exports = React.createClass({
         {this.state.name}
       </h3>
       <span className={badgeClasslist.join(" ")}> {name} </span>
-      <span className="divider"></span>
-      <span className="percent">0%</span>
       {controls}
     </div>
   }
