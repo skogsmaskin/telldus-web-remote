ac6fcfa4eb75832c2f8197c8b2cbce8a8e279c76
Bjørge Næss <bjoerge@bengler.no>
2014-01-20 22:47:42 +0100

    Bootstrap js on domready

--- client/bootstrap-client.jsx
+++ client/bootstrap-client.jsx
@@ -6,5 +6,9 @@ var DeviceList = require("./elements/device-list");
 
 var api = require("./data/api");
 
-var initial = JSON.parse(document.getElementById("deviceListInitialData").innerHTML);
-React.renderComponent(<DeviceList devices={initial}/>, document.getElementById('container'));
+var domReady = require("domready");
+
+domReady(function() {
+  var initial = JSON.parse(document.getElementById("deviceListInitialData").innerHTML);
+  React.renderComponent(<DeviceList devices={initial}/>, document.getElementById('container'));
+});
